2223a824043cfa628b503e8558f3aee067cda8be
Hero <q_hero@outlook.com>
2018-01-24 22:07:03 +0000

    adding comment on method

--- ObserverPattern/WeatherStation.cs
+++ ObserverPattern/WeatherStation.cs
@@ -13,6 +13,7 @@ namespace ObserverPattern
             var forecastDisplay = new ForecastDisplay(weatherData);
             var statsDisplay = new StatisticsDisplay(weatherData);
 
+            //simulating sending data 
             weatherData.SetMeasurements(20.1f, 70, 104.2f);
             weatherData.SetMeasurements(18.5f, 80, 101.3f);
             weatherData.SetMeasurements(18.0f, 75, 101.3f);
