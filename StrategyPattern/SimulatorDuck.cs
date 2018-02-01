0b285d8d36966d471200a03743dd176c3081dd68
Hero <q_hero@outlook.com>
2018-02-01 14:01:04 +0000

    oups forgot adding new project and the named files SimulatorDuck to previous commit

---
+++ StrategyPattern/SimulatorDuck.cs
@@ -0,0 +1,20 @@
+﻿using StrategyPattern.Behaviors;
+using StrategyPattern.Entities;
+using System;
+
+namespace StrategyPattern
+{
+    public class SimulatorDuck
+    {
+        public static void Main(string[] args)
+        {
+            Duck model = new ModelDuck();
+
+            model.PerformFly();
+            model.SetFlyBehavior(new FlyRocketPowered());
+            model.PerformFly();
+
+            Console.ReadLine();
+        }
+    }
+}
