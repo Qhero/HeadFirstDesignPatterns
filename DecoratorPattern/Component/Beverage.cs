5a76409bb29ba5e3728459d8055bceefba8849db
Hero <q_hero@outlook.com>
2018-02-12 21:08:21 +0000

    Adding the DecoratorPattern code.

---
+++ DecoratorPattern/Component/Beverage.cs
@@ -0,0 +1,22 @@
+﻿namespace DecoratorPattern.Component
+{
+    public abstract class Beverage
+    {
+        protected string description = "Unknow Beverage";
+
+        /// <summary>
+        /// Description of the beverage
+        /// </summary>
+        /// <returns></returns>
+        public virtual string GetDescription()
+        {
+            return description;
+        }
+
+        /// <summary>
+        /// Cost of the beverage.
+        /// </summary>
+        /// <returns></returns>
+        public abstract double Cost();
+    }
+}
---
+++ DecoratorPattern/ConcreteComponents/Decaf.cs
@@ -0,0 +1,17 @@
+﻿using DecoratorPattern.Component;
+
+namespace DecoratorPattern.ConcreteComponents
+{
+    public class Decaf : Beverage
+    {
+        public Decaf()
+        {
+            description = "Decaf Coffee";
+        }
+
+        public override double Cost()
+        {
+            return 1.05;
+        }
+    }
+}
